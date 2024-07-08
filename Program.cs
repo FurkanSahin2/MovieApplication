59a997f93a5fbcdc09a2597a9eaa504381739f36
Furkan Sahin <aliince.000000@gmail.com>
2024-07-08 23:49:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Channels;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace ConsoleUI
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              // MovieTest();
+        }
+         
+        private static void MovieTest()
+        {
+            MovieManager movieManager = new MovieManager(new EfMovieDal());
+
+            var result = movieManager.GetMovieDetails();
+
+            if (result.Success == true)
+            {
+                foreach (var movie in result.Data)
+                {
+                    Console.WriteLine(movie.MovieName + " / " + movie.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+           
+        }
+    }
+}
+
